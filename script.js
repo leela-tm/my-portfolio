917baa4d45d195dc1cf86dff0448407246c3d3fa
leelabari <leelabari.tombari-menegbo@pau.edu.ng>
2026-01-01 21:03:19 +0100

    changes

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ function scrollToContact() {
   });
 }
 
-// Simple form alert
-document.querySelector("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Message sent! (This is a demo)");
+AOS.init({
+  duration: 1000,
+  delay: 0,
+  offset: 400,
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      alert("Message sent! (This is a demo)");
+    });
+  }
 });
